ffe9bff374ea9a1b48bff3e892a0bc04fe92179b
Olaf <github@dacht.net>
2023-06-28 20:12:02 +0200

    Initial config with homerow mod...

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# My QMK variations and resources.
+
+As I go on my journey on keyboard mapping and modding with QMK. I've created this repository.
+
+It relies on the directories under users being logical linked to link under the users directory  in the QMK directory tree.
+And the keymaps linked to the appropriate and similar locations in the keyboards/<brand>/<type>/keymap directory. 
+
+For instance
+```
+cd qmk_firmware/keyboards/splitkb/kyria/keymap
+ln -s ./../../../../../QMK-variations/keyboards/splitkb/kyria/keymaps/kolkman ./
+```
+
+ ### Some useful resources.
+ - [keymapdb](https.keymapdb.com), in particular the [kyria keymap](https://keymapdb.com/keymaps/default-kyria/).
+ - https://precondition.github.io/
--- keyboards/splitkb/kyria/keymaps/kolkman/keymap.c
+++ keyboards/splitkb/kyria/keymaps/kolkman/keymap.c
@@ -45,9 +45,20 @@ enum {
 #define CTL_MINS MT(MOD_RCTL, KC_MINUS)
 #define ALT_ENT  MT(MOD_LALT, KC_ENT)
 
+
 // Note: LAlt/Enter (ALT_ENT) is not the same thing as the keyboard shortcut Alt+Enter.
 // The notation `mod/tap` denotes a key that activates the modifier `mod` when held down, and
 // produces the key `tap` when tapped (i.e. pressed and released).
+
+
+// See https://precondition.github.io/home-row-mods
+// Using a CAGS layout
+// Control, ALT (Option), GUI (Command), Shift
+// Results in
+// LCTL_T(KC_A) ,  LALT_T(KC_S)  ,  LGUI_T(KC_D)  ,  LSFT_T(KC_F) 
+// RSFT_T(KC_J) ,  RGUI_T(KC_K),   LALT_T(KKC_L) , RCTL_T(K_SCLN) 
+
+
 
 
 
@@ -74,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_QWERTY] = LAYOUT(
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
-     CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L , TD(TD_SCLN_ENT) ,CTL_QUOT,
+     CTL_ESC , LCTL_T(KC_A) ,  LALT_T(KC_S)  ,  LGUI_T(KC_D)  ,  LSFT_T(KC_F) ,   KC_G ,           KC_H,   RSFT_T(KC_J) ,  RGUI_T(KC_K),   LALT_T(KC_L) , RCTL_T(KC_SCLN) ,CTL_QUOT,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
                                 ADJUST , KC_LGUI, ALT_ENT, KC_SPC , NAV   ,     SYM    , KC_SPC ,KC_RALT, KC_RGUI, KC_APP
     ),
