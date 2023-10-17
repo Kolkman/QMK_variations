16f7895be116fc9671741d7e2c15bcf2769ade14
Olaf <github@dacht.net>
2023-10-17 12:47:02 +0200

    parentheses on symbol layer above square brackets

--- keyboards/splitkb/kyria/keymaps/kolkman/keymap.c
+++ keyboards/splitkb/kyria/keymaps/kolkman/keymap.c
@@ -178,7 +178,7 @@ tap_dance_action_t tap_dance_actions[] = {
           [_SYM] = LAYOUT(
           KC_GRV ,   KC_1 ,   KC_2 ,   KC_3 ,   KC_4 ,   KC_5 ,                                       KC_6 ,   KC_7 ,   KC_8 ,   KC_9 ,   KC_0 , KC_EQL ,
           KC_TILD , KC_EXLM,  KC_AT , KC_HASH,  KC_DLR, KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PLUS,
-          KC_PIPE , KC_BSLS, KC_COLN, KC_SCLN, KC_MINS, KC_LBRC, KC_LCBR, _______, _______, KC_RCBR, KC_RBRC, KC_UNDS, KC_COMM,  KC_DOT, KC_SLSH, KC_QUES,
+          KC_PIPE , KC_BSLS, KC_COLN, KC_SCLN, KC_MINS, KC_LBRC, KC_LPRN, _______, _______, KC_RPRN, KC_RBRC, KC_UNDS, KC_COMM,  KC_DOT, KC_SLSH, KC_QUES,
                                       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
           ),
 
--- keyboards/splitkb/kyria/keymaps/kolkman/readme.md
+++ keyboards/splitkb/kyria/keymaps/kolkman/readme.md
@@ -6,6 +6,7 @@
 *   Simultaneous space press activates CAPS_WORDS.
 *   Control Option Command Shift - [Modrow](https://precondition.github.io/home-row-mods) (Only on the QWERTY keyboard)
 *   Removed  CTRL on hold from the quote key.
+*   Parentheses on the square brackets symbol layer
 *   Option Command order on the thumb row swapped.
 *   Power led [on for Caps lock](https://docs.splitkb.com/hc/en-us/articles/5799711553820-Power-LED).
 *   RGB lightlayers, see the [feature_rgblight.md](https://github.com/qmk/qmk_firmware/blob/master/docs/feature_rgblight.md) with command option shift and control providing light effects.
